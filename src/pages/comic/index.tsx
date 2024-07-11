248b33650f235f7142115615c844b75f8e1173bb
Quartz_Admirer <118068910+Quartz-Admirer@users.noreply.github.com>
2024-07-11 22:29:09 +0300

    Update index.tsx

--- src/pages/comic/index.tsx
+++ src/pages/comic/index.tsx
@@ -1,39 +1,34 @@
 import React from 'react';
-import axios from 'axios';
-import '../../styles/globals.css';
-
-import ComicPage from '../../components/ComicPage';
+import '../styles/globals.css';
+import Head from 'next/head';
 import NavComponent from "@/components/NavComponent";
-
-type Comic = {
-    safe_title: string;
-    img: string;
-    alt: string;
-    year: string;
-    month: string;
+import Image from "next/image";
+export const metadata = {
+    title: 'Oleg Shchendrigin',
+    description: 'Graphic Designer Portfolio',
 };
 
-type ComicPageProps = {
-    comic: Comic;
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    return (
+        <html lang="en">
+        <Head>
+            <title>{metadata.title}</title>
+            <meta name="description" content={metadata.description} />
+            <meta name="author" content="Oleg Shchendrigin" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+        <div>
+            <header>
+                <Image width={1000} height={1000} src="/ava_wb.jpg" className="avatar" alt="Oleg Shchendrigin" />
+                <h1>Oleg Shchendrigin</h1>
+            </header>
+            <NavComponent />
+            <main>{children}</main>
+        </div>
+        </body>
+        </html>
+    );
 };
 
-const fetchComicData = async (): Promise<Comic> => {
-    const email = 'o.shchendrigin@innopolis.university';
-    const emailParam = new URLSearchParams({ email });
-    const comicIdResponse = await axios.get(`https://fwd.innopolis.university/api/hw2?email=${emailParam}`);
-    const comicId = comicIdResponse.data;
-
-    const comicResponse = await axios.get(`https://fwd.innopolis.university/api/comic?id=${comicId}`);
-    return comicResponse.data;
-};
-
-const ComicPageWrapper: React.FC<ComicPageProps> = ({ comic }) => {
-    return (<><NavComponent/><ComicPage comic={comic}/></>);
-};
-
-export async function getServerSideProps() {
-    const comicData = await fetchComicData();
-    return { props: { comic: comicData } };
-}
-
-export default ComicPageWrapper;
+export default Layout;
